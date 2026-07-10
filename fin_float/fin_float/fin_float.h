0a9dcf309150c3673773bb2f4ccf20c6d30ea7dc
IceFRain <icefrain@163.com>
2026-07-10 15:34:22 +0800

    添加qlog处理、悬浮窗移动处理

--- fin_float/fin_float/fin_float.h
+++ fin_float/fin_float/fin_float.h
@@ -2,6 +2,8 @@
 #define FINFLOAT_H
 
 #include <QWidget>
+#include <QDebug>
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class FinFloat; }
@@ -15,7 +17,21 @@ public:
     FinFloat(QWidget *parent = nullptr);
     ~FinFloat();
 
+signals:
+    //双击事件
+    void sig_double_clicked(void);
+
+protected:
+    //鼠标点击事件
+    void mousePressEvent(QMouseEvent *event) override;
+    //鼠标移动事件
+    void mouseMoveEvent(QMouseEvent *event) override;
+    //鼠标双击事件
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
+
 private:
     Ui::FinFloat *ui;
+    //记录鼠标位置,移动悬浮窗使用
+    QPoint m_drag_position{0,0};
 };
 #endif // FINFLOAT_H
