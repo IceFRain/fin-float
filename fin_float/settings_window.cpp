5bd86c043b9049c06838ca020b96bb5dcd88b2e2
IceFRain <icefrain@163.com>
2026-07-13 13:37:36 +0800

    设置界面完成

--- fin_float/settings_window.cpp
+++ fin_float/settings_window.cpp
@@ -31,12 +31,28 @@ SettingsWindow::SettingsWindow(QWidget *parent) :
     connect(ui->listWidget_setting_group,&QListWidget::currentRowChanged,ui->stackedWidget_settings,&QStackedWidget::setCurrentIndex);
 
     //设置按钮连接
+    connect(ui->pushButton_show_change_cancel,&QPushButton::clicked,this,&SettingsWindow::slot_pb_change_cancel_clicked);
+    connect(ui->pushButton_show_change_save,&QPushButton::clicked,this,&SettingsWindow::slot_pb_change_save_clicked);
+    connect(ui->pushButton_target_add,&QPushButton::clicked,this,&SettingsWindow::slot_pb_target_add_clicked);
+    connect(ui->pushButton_target_del,&QPushButton::clicked,this,&SettingsWindow::slot_pb_target_del_clicked);
     connect(ui->pushButton_project_url_github,&QPushButton::clicked,this,&SettingsWindow::slot_pb_project_url_github_clicked);
     connect(ui->pushButton_project_url_gitee,&QPushButton::clicked,this,&SettingsWindow::slot_pb_project_url_gitee_clicked);
     connect(ui->pushButton_reset_settings,&QPushButton::clicked,this,&SettingsWindow::slot_pb_reset_settings_clicked);
 
     //默认选择第一组设置项
     ui->listWidget_setting_group->setCurrentRow(0);
+    //设置表头
+    ui->treeWidget_target->setHeaderLabels(QStringList() << "显示" << "代码" << "名称");
+    ui->treeWidget_target->headerItem()->setTextAlignment(0, Qt::AlignCenter);
+    ui->treeWidget_target->header()->setSectionResizeMode(0, QHeaderView::Fixed);
+    ui->treeWidget_target->headerItem()->setTextAlignment(1, Qt::AlignCenter);
+    ui->treeWidget_target->headerItem()->setTextAlignment(2, Qt::AlignCenter);
+    ui->treeWidget_target->setColumnWidth(0, 58);
+    ui->treeWidget_target->setColumnWidth(1, 150);
+    ui->treeWidget_target->header()->setStretchLastSection(true);  // 最后一列自动填满
+
+    //读取配置文件
+    read_settings_from_file();
 }
 
 /**
@@ -91,34 +107,28 @@ void SettingsWindow::read_settings_from_file()
         }
     }
 
-//    QJsonObject clock  = root["clock"].toObject();
-//    QJsonObject drink  = root["drink"].toObject();
-//    QJsonObject notice = root["notice"].toObject();
+    QJsonObject show  = root["show"].toObject();
+    m_settings.show.api = show["api"].toInt(0);
+    m_settings.show.orientation = show["orientation"].toInt(0);
+    m_settings.show.mode = show["mode"].toInt(0);
+    m_settings.show.line_width = show["line_width"].toInt(1);
+    m_settings.show.show_on_hover = show["show_on_hover"].toInt(1);
+    m_settings.show.font_family = show["font_family"].toString("MiSans");
+    m_settings.show.font_point_size = show["font_point_size"].toInt(12);
+    m_settings.show.block_width = show["block_width"].toInt(10);
+    m_settings.show.block_height = show["block_height"].toInt(20);
 
-//    m_settings.clock.work_time     = clock["work_time"].toInt(25);
-//    m_settings.clock.rest_time     = clock["rest_time"].toInt(5);
-//    m_settings.clock.floating_show = clock["floating_show"].toInt(1);
-
-//    m_settings.drink.goal          = drink["goal"].toInt(1800);
-//    m_settings.drink.cup_capacity  = drink["cup_capacity"].toInt(500);
-//    m_settings.drink.record        = drink["record"].toInt(0);
-//    m_settings.drink.floating_show = drink["floating_show"].toInt(1);
-//    m_settings.drink.record_date   = QDate::fromString(drink["record_date"].toString(), Qt::ISODate);
-//    QDate today = QDate::currentDate();
-//    if (!m_settings.drink.record_date.isValid() || m_settings.drink.record_date != today)
-//    {
-//        m_settings.drink.record_date = QDate::currentDate();
-//        m_settings.drink.record = 0;
-//    }
-
-//    m_settings.notice.clock_notice_enable  = notice["clock_notice_enable"].toInt(1);
-//    m_settings.notice.drink_notice_enable  = notice["drink_notice_enable"].toInt(1);
-//    m_settings.notice.messge_box_enable    = notice["message_box_enable"].toInt(0);
-//    m_settings.notice.sys_notice_enable    = notice["sys_notice_enable"].toInt(1);
-//    m_settings.notice.sys_notice_keep_time = notice["sys_notice_keep_time"].toInt(30);
-//    m_settings.notice.clock_work_over_tips = notice["clock_work_over_tips"].toString("休息一下~");
-//    m_settings.notice.clock_rest_over_tips = notice["clock_rest_over_tips"].toString("干活~");
-//    m_settings.notice.get_drink_goal_tips  = notice["get_drink_goal_tips"].toString("达成今日喝水目标~");
+    m_settings.target.clear();
+    QJsonArray targetArray = root["target"].toArray();
+    for (const QJsonValue &val : targetArray)
+    {
+        QJsonObject obj = val.toObject();
+        TargetInfo info;
+        info.enabled  = obj["enabled"].toInt(0);
+        info.code     = obj["code"].toString();
+        info.name     = obj["name"].toString();
+        m_settings.target.append(info);
+    }
 
     init_ui_by_settings();
 }
@@ -131,41 +141,133 @@ void SettingsWindow::read_settings_from_file()
  */
 void SettingsWindow::save_settings_to_file()
 {
-//    QJsonObject clock;
-//    clock["work_time"]     = m_settings.clock.work_time;
-//    clock["rest_time"]     = m_settings.clock.rest_time;
-//    clock["floating_show"] = m_settings.clock.floating_show;
+    //从界面读取当前的配置
+    m_settings.show.api = ui->comboBox_show_api->currentIndex();
+    m_settings.show.orientation = ui->comboBox_show_orientation->currentIndex();
+    m_settings.show.mode = ui->comboBox_show_mode->currentIndex();
+    m_settings.show.line_width = ui->spinBox_show_line_width->value();
+    m_settings.show.show_on_hover = ui->checkBox_show_on_hover->isChecked();
+    m_settings.show.font_family = ui->comboBox_font_family->currentText();
+    m_settings.show.font_point_size = ui->comboBox_font_point_size->currentText().toInt();
+    m_settings.show.block_width = ui->spinBox_show_block_width->value();
+    m_settings.show.block_height = ui->spinBox_show_block_height->value();
+    //读取标的列表
+    m_settings.target.clear();
+    for (int i = 0; i < ui->treeWidget_target->topLevelItemCount(); i++)
+    {
+        QTreeWidgetItem *item = ui->treeWidget_target->topLevelItem(i);
+        TargetInfo info;
+        info.enabled = (item->checkState(0) == Qt::Checked) ? 1 : 0;
+        info.code    = item->text(1);
+        info.name    = item->text(2);
+        m_settings.target.append(info);
+    }
 
-//    QJsonObject drink;
-//    drink["goal"]          = m_settings.drink.goal;
-//    drink["cup_capacity"]  = m_settings.drink.cup_capacity;
-//    drink["record"]        = m_settings.drink.record;
-//    drink["floating_show"] = m_settings.drink.floating_show;
-//    drink["record_date"]   = m_settings.drink.record_date.toString(Qt::ISODate);
+    //show 部分
+    QJsonObject show;
+    show["api"]            = m_settings.show.api;
+    show["orientation"]    = m_settings.show.orientation;
+    show["mode"]           = m_settings.show.mode;
+    show["line_width"]     = m_settings.show.line_width;
+    show["show_on_hover"]  = m_settings.show.show_on_hover;
+    show["font_family"]    = m_settings.show.font_family;
+    show["font_point_size"]= m_settings.show.font_point_size;
+    show["block_width"]    = m_settings.show.block_width;
+    show["block_height"]   = m_settings.show.block_height;
 
-//    QJsonObject notice;
-//    notice["clock_notice_enable"]   = m_settings.notice.clock_notice_enable;
-//    notice["drink_notice_enable"]   = m_settings.notice.drink_notice_enable;
-//    notice["message_box_enable"]   = m_settings.notice.messge_box_enable;
-//    notice["sys_notice_enable"]    = m_settings.notice.sys_notice_enable;
-//    notice["sys_notice_keep_time"] = m_settings.notice.sys_notice_keep_time;
-//    notice["clock_work_over_tips"] = m_settings.notice.clock_work_over_tips;
-//    notice["clock_rest_over_tips"] = m_settings.notice.clock_rest_over_tips;
-//    notice["get_drink_goal_tips"]  = m_settings.notice.get_drink_goal_tips;
+    //target 部分
+    QJsonArray targetArray;
+    for (const TargetInfo &info : m_settings.target)
+    {
+        QJsonObject obj;
+        obj["enabled"] = info.enabled;
+        obj["code"]    = info.code;
+        obj["name"]    = info.name;
+        targetArray.append(obj);
+    }
 
-//    QJsonObject root;
-//    root["clock"]  = clock;
-//    root["drink"]  = drink;
-//    root["notice"] = notice;
+    //组装根对象
+    QJsonObject root;
+    root["show"]   = show;
+    root["target"] = targetArray;
 
-//    QFile file("settings.json");
-//    if (!file.open(QIODevice::WriteOnly))
-//    {
-//        qDebug()<<"settings.json open failed";
-//        return;
-//    }
-//    file.write(QJsonDocument(root).toJson(QJsonDocument::Indented));
-//    file.close();
+    //写文件
+    QFile file("settings.json");
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        qDebug() << "settings.json save failed";
+        return;
+    }
+    file.write(QJsonDocument(root).toJson());
+    file.close();
+}
+
+/**
+  * @brief 设置保存按钮点击槽
+  * @param 无
+  * @retval 无
+  * 	@arg
+ */
+void SettingsWindow::slot_pb_change_save_clicked()
+{
+    //二次确认
+    QMessageBox::StandardButton btn = QMessageBox::question(
+        this, "确认", "要保存更改吗？");
+
+    if (btn == QMessageBox::Yes)
+    {
+        save_settings_to_file();
+    }
+}
+
+/**
+  * @brief 设置取消保存按钮点击槽
+  * @param 无
+  * @retval 无
+  * 	@arg
+ */
+void SettingsWindow::slot_pb_change_cancel_clicked()
+{
+    //二次确认
+    QMessageBox::StandardButton btn = QMessageBox::question(
+        this, "确认", "要撤销所有更改吗？");
+
+    if (btn == QMessageBox::Yes)
+    {
+        read_settings_from_file();
+    }
+}
+
+/**
+  * @brief 增加标的点击槽
+  * @param 无
+  * @retval 无
+  * 	@arg
+ */
+void SettingsWindow::slot_pb_target_add_clicked()
+{
+    QTreeWidgetItem *item = new QTreeWidgetItem(ui->treeWidget_target);
+    item->setCheckState(0,Qt::Checked);
+    item->setText(1,"000000");
+    item->setText(2,"");
+    item->setTextAlignment(1, Qt::AlignCenter);
+    item->setTextAlignment(2, Qt::AlignCenter);
+    item->setFlags(item->flags() | Qt::ItemIsEditable);
+}
+
+/**
+  * @brief 删除标的点击槽
+  * @param 无
+  * @retval 无
+  * 	@arg
+ */
+void SettingsWindow::slot_pb_target_del_clicked()
+{
+    QTreeWidgetItem *item = ui->treeWidget_target->currentItem();
+    if (item)
+    {
+        delete item;
+    }
 }
 
 /**
@@ -207,8 +309,83 @@ void SettingsWindow::slot_pb_reset_settings_clicked()
         //删除当前设置文件后,重新读取数据,设置为默认值
         QFile::remove("./settings.json");
         read_settings_from_file();
-        save_settings_to_file();
         emit sig_settings_changed();
+    }
+}
+
+/**
+  * @brief 重写关闭事件
+  * @param 无
+  * @retval 无
+  * 	@arg
+ */
+void SettingsWindow::closeEvent(QCloseEvent *event)
+{
+    uint8_t changed_flag=0;
+    //先比较标的列表
+    if(ui->treeWidget_target->topLevelItemCount() != m_settings.target.size())
+    {
+        changed_flag = 1;
+    }
+    else
+    {
+        for (int i = 0; i < ui->treeWidget_target->topLevelItemCount(); i++)
+        {
+            QTreeWidgetItem *item = ui->treeWidget_target->topLevelItem(i);
+            TargetInfo &info = m_settings.target[i];
+            if(     info.enabled != ((item->checkState(0) == Qt::Checked) ? 1 : 0)
+                ||  info.code != item->text(1)
+                ||  info.name != item->text(2))
+            {
+                changed_flag = 1;
+                break;
+            }
+        }
+    }
+    //再比较标的列表外的
+    if(!changed_flag)
+    {
+        if(m_settings.show.api != ui->comboBox_show_api->currentIndex()){
+            changed_flag = 1;
+        }else if(m_settings.show.orientation != ui->comboBox_show_orientation->currentIndex()){
+            changed_flag = 1;
+        }else if(m_settings.show.mode != ui->comboBox_show_mode->currentIndex()){
+            changed_flag = 1;
+        }else if(m_settings.show.line_width != ui->spinBox_show_line_width->value()){
+            changed_flag = 1;
+        }else if(m_settings.show.show_on_hover != ui->checkBox_show_on_hover->isChecked()){
+            changed_flag = 1;
+        }else if(m_settings.show.font_family != ui->comboBox_font_family->currentText()){
+            changed_flag = 1;
+        }else if(m_settings.show.font_point_size != ui->comboBox_font_point_size->currentText().toInt()){
+            changed_flag = 1;
+        }else if(m_settings.show.block_width != ui->spinBox_show_block_width->value()){
+            changed_flag = 1;
+        }else if(m_settings.show.block_height != ui->spinBox_show_block_height->value()){
+            changed_flag = 1;
+        }
+    }
+
+    //存在修改则提示是否保存
+    if (changed_flag)
+    {
+        QMessageBox::StandardButton btn = QMessageBox::question(
+            this, "确认", "设置已修改,是否保存?");
+
+        if (btn == QMessageBox::Yes)
+        {
+            save_settings_to_file();
+            event->accept();
+        }
+        else
+        {
+            read_settings_from_file();
+            event->accept();
+        }
+    }
+    else
+    {
+        event->accept();
     }
 }
 
@@ -221,4 +398,49 @@ void SettingsWindow::slot_pb_reset_settings_clicked()
  */
 void SettingsWindow::init_ui_by_settings()
 {
+    //添加设置项
+    ui->comboBox_show_api->clear();
+    ui->comboBox_show_api->addItem("sina",API_SINA);
+    ui->comboBox_show_api->addItem("east",API_EAST);
+    ui->comboBox_show_orientation->clear();
+    ui->comboBox_show_orientation->addItem("横向",ORIENTATION_H);
+    ui->comboBox_show_orientation->addItem("纵向",ORIENTATION_V);
+    ui->comboBox_show_mode->clear();
+    ui->comboBox_show_mode->addItem("图形",MODE_GRAPHICAL);
+    ui->comboBox_show_mode->addItem("数字",MODE_NUMERIC);
+    //字体设置项
+    QFontDatabase fontDatabase;
+    QStringList families = fontDatabase.families();
+    ui->comboBox_font_family->clear();
+    ui->comboBox_font_family->addItems(families);
+    QStringList sizes;
+    sizes << "6" << "7" << "8" << "9" << "10" << "10.5" << "11" << "12"
+          << "14" << "16" << "18" << "20" << "22" << "24" << "26" << "28";
+    ui->comboBox_font_point_size->clear();
+    ui->comboBox_font_point_size->addItems(sizes);
+
+    //按读取到的配置进行初始化
+    ui->comboBox_show_api->setCurrentIndex(m_settings.show.api);
+    ui->comboBox_show_orientation->setCurrentIndex(m_settings.show.orientation);
+    ui->comboBox_show_mode->setCurrentIndex(m_settings.show.mode);
+    ui->spinBox_show_line_width->setValue(m_settings.show.line_width);
+    ui->checkBox_show_on_hover->setChecked(m_settings.show.show_on_hover);
+    ui->comboBox_font_family->setCurrentText(m_settings.show.font_family);
+    ui->comboBox_font_point_size->setCurrentText(QString::number(m_settings.show.font_point_size));
+    ui->spinBox_show_block_width->setValue(m_settings.show.block_width);
+    ui->spinBox_show_block_height->setValue(m_settings.show.block_height);
+
+    //填充标的数据
+    ui->treeWidget_target->clear();
+    ui->treeWidget_target->setHeaderLabels(QStringList() << "显示" << "代码" << "名称");
+    for (const TargetInfo &info : m_settings.target)
+    {
+        QTreeWidgetItem *item = new QTreeWidgetItem(ui->treeWidget_target);
+        item->setCheckState(0, info.enabled ? Qt::Checked : Qt::Unchecked);
+        item->setText(1, info.code);
+        item->setText(2, info.name);
+        item->setTextAlignment(1, Qt::AlignCenter);
+        item->setTextAlignment(2, Qt::AlignCenter);
+        item->setFlags(item->flags() | Qt::ItemIsEditable);
+    }
 }
--- fin_float/settings_window.h
+++ fin_float/settings_window.h
@@ -11,21 +11,36 @@
 #include <QUrl>
 #include <QMessageBox>
 #include <QDate>
+#include <QFontDatabase>
+#include <QComboBox>
+#include <QCloseEvent>
+
+//API类型
+enum DATA_API{API_SINA,API_EAST};
+//显示横纵向
+enum SHOW_ORIENTATION{ORIENTATION_H,ORIENTATION_V};
+//显示模式
+enum SHOW_MODE{MODE_GRAPHICAL,MODE_NUMERIC};
 
 //显示设置项结构体
 typedef struct
 {
-    short api;              //数据源选择
-    short orientation;      //显示方向
-    short mode;
-    short line_width;
+    short api;                          //数据源选择 east,sina
+    short orientation;                  //显示方向 横向,纵向
+    short mode;                         //显示模式 图形,数字
+    short line_width;                   //图形线宽
+    short show_on_hover;                //悬停显示开关
+    QString font_family;                //数字字体
+    short font_point_size;              //数字字体大小
+    short block_width;                  //显示块宽度
+    short block_height;                 //显示块高度
 }ShowSettings;
 //标的结构体
 typedef struct
 {
-    short enabled;
-    QString code;
-    QString name;
+    short enabled;                      //当前标的是否需要显示
+    QString code;                       //当前标的代码
+    QString name;                       //当前标的名称,自动获取
 }TargetInfo;
 //设置项结构体
 typedef struct
@@ -33,7 +48,6 @@ typedef struct
     ShowSettings show;
     QVector<TargetInfo> target;
 }AllSettings;
-
 
 #define SOFT_VERSION    "V0.0.1"
 
@@ -56,6 +70,14 @@ public:
     void save_settings_to_file(void);
 
 public slots:
+    //设置保存按钮点击槽
+    void slot_pb_change_save_clicked(void);
+    //设置取消保存按钮点击槽
+    void slot_pb_change_cancel_clicked(void);
+    //增加标的点击槽
+    void slot_pb_target_add_clicked(void);
+    //删除标的点击槽
+    void slot_pb_target_del_clicked(void);
     //项目官网按钮点击槽
     void slot_pb_project_url_gitee_clicked(void);
     void slot_pb_project_url_github_clicked(void);
@@ -65,6 +87,9 @@ public slots:
 signals:
     //设置改变信号
     void sig_settings_changed(void);
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
 
 private:
     Ui::SettingsWindow *ui;
